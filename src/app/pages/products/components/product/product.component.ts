f5d3e98692fb64171a3e4c8660e68ece8cf8ccef
ahmed ramzy <Ahmedramzy_fcih@yahoo.com>
2024-06-08 22:55:58 +0300

    add update quantity ui and integration

--- src/app/pages/products/components/product/product.component.ts
+++ src/app/pages/products/components/product/product.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, inject } from '@angular/core';
 import { Product } from '../../models/product.interface';
 import { CommonModule, CurrencyPipe } from '@angular/common';
+import { ProductsService } from '../../../../@cores/services/products/products.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-product',
@@ -9,7 +11,25 @@ import { CommonModule, CurrencyPipe } from '@angular/common';
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
-export class ProductComponent {
+export class ProductComponent implements OnDestroy{
+
   // data is required for product component
-  @Input({required:true}) data!:Product
+  @Input({required:true}) data!:Product;
+
+  notifier = new Subject();
+  _productService=inject(ProductsService)
+  update(productId:number,quantity:number){
+    this._productService.editProductQuantity(productId,quantity).pipe(
+      takeUntil(this.notifier)
+    ).subscribe({
+      next:(res:any)=>{
+        this.data=res
+        console.log(res)
+      }
+    })
+  }
+  ngOnDestroy(): void {
+    this.notifier.next(true)
+    this.notifier.complete()
+  }
 }
