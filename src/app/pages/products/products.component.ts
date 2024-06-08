4c532c027b1beb0d03d393d60a250d9f936ccc69
ahmed ramzy <Ahmedramzy_fcih@yahoo.com>
2024-06-08 16:30:05 +0300

    add product ui

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ProductsService } from '../../@cores/services/products/products.service';
+import { Product } from './models/product.interface';
+import { ProductComponent } from './components/product/product.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent],
+  providers:[ProductsService],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  _productService=inject(ProductsService);
+  products:Product[]=[]
+  ngOnInit(): void {
+    this.getProducts()
+  }
+  getProducts(){
+    this._productService.getProducts().subscribe({
+      next:(res:Product[])=>{
+        this.products=res
+      }
+    });
+  }
 
 }
